84ebd62518d91409f4c5e4e6b6c855df8a16a00f
martijnbrands <nijtrambra@hotmail.com>
2018-02-28 16:13:31 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    //$('a-scene').append( '<a-box position="-2.5 2.5 0" rotation="0 0 0" width="1" color="red"></a-box>');
+    // require('aframe-leap-hands').registerAll();
+});
+
