fbbde995c2191bd321566a1123d05302d421e3fd
martijnbrands <nijtrambra@hotmail.com>
2018-03-10 16:55:55 +0100

    start with islands

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
-$(document).ready(function(){
-    $('#splash').remove();
-});
+var one = false;
+var two = false;
+
+
+function addOne() {
+
+    console.log("test");
+    var one = true
+    checkstatus();
+
+}
+function addTwo() {
+	console.log(two)
+    console.log("test2");
+    var two = true
+    checkstatus();
+}
+
+// function checkstatus() {
+// 	if (addOne() === true && addTwo() === true){
+//     console.log("worked");
+// 	}else{
+// 		console.log("false");
+// 	}
+// }
+
+function checkstatus() {
+   	if (one == true && two == true){
+    	console.log("worked");
+	}
+}
