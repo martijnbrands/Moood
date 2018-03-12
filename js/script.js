324442f543c81ed83ac69828c9b919c22bc0cd25
martijnbrands <nijtrambra@hotmail.com>
2018-03-12 10:59:22 +0100

    purple world

--- js/script.js
+++ js/script.js
@@ -1,31 +1,19 @@
-var one = false;
-var two = false;
 
+   	let seenObjectOne = false;
+	let seenObjectTwo = false;
 
-function addOne() {
-
-    console.log("test");
-    var one = true
-    checkstatus();
-
-}
-function addTwo() {
-	console.log(two)
-    console.log("test2");
-    var two = true
-    checkstatus();
-}
-
-// function checkstatus() {
-// 	if (addOne() === true && addTwo() === true){
-//     console.log("worked");
-// 	}else{
-// 		console.log("false");
-// 	}
-// }
-
-function checkstatus() {
-   	if (one == true && two == true){
-    	console.log("worked");
+	function addOne() {
+	    seenObjectOne = true;
+	    checkStatus();
 	}
-}
+
+	function addTwo() {
+	    seenObjectTwo = true;
+	    checkStatus();
+	}
+
+	function checkStatus() {
+	       if(seenObjectOne && seenObjectTwo){
+	        console.log("Well done!");
+	    }
+	}
