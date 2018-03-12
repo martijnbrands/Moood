89a6c485ea84913d6d5763cef45f26ebba51507d
martijnbrands <nijtrambra@hotmail.com>
2018-03-12 20:53:46 +0100

    Add splash screen

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+$( document ).ready(function() {
+    $("#start-btn").click(function(){
+        $(".splash-screen").fadeOut( "slow" );
+    });
+});
+
 let sphereSeen = false;
 let boxSeen = false;
 let octahedronSeen = false;
@@ -20,7 +26,26 @@ function octahedronActive() {
 function checkActive() {
 	if(sphereSeen && boxSeen && octahedronSeen){
 	    console.log("Well done!");
-	    var sceneEl = document.querySelector('a-scene');
-			sceneEl.querySelector('#next-level').setAttribute("visible", "true");;
+	    let sceneEl = document.querySelector('a-scene');
+			sceneEl.querySelector('#next-level').setAttribute("visible", "true");
 	}
+}
+
+
+let numBalloons = 0;
+
+function addBalloon() {
+    if (numBalloons == 3) {
+       console.log("Alle ballonnen");
+    }
+    else {
+    	console.log("Add ballonnen");
+    	balloon = document.createElement("a-box")
+    	balloon.setAttribute("color", "#000")
+        balloon.setAttribute("position", "0 14 -4")
+
+        document.getElementById("blokhut").appendChild(balloon);
+        numBalloons += 1
+        console.log(numBalloons);
+    }
 }
