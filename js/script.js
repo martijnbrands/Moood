dce912960b89f4615ea10641f503ebcd0d41362a
martijnbrands <nijtrambra@hotmail.com>
2018-03-13 15:59:30 +0100

    Add balloons working

--- js/script.js
+++ js/script.js
@@ -38,20 +38,58 @@ function checkActive() {
 }
 
 
-let numBalloons = 0;
+
+
+let numBalloons = 0
 
 function addBalloon() {
-    if (numBalloons == 3) {
-       console.log("Alle ballonnen");
+    // for (var i = 0; i < 25; i++) {
+    //     let triggerPosX = Math.random() * 6 -3; // tussen 3 en -3 // tussen 3 en -3
+    //     let triggerPosY = 12 // staat vast
+    //     let triggerPosZ = Math.floor(Math.random() * 3) + 0.6; // tussen 0.6 en 3
+
+    //     let balloonPosX = Math.random() * 2 + 1; //tussen 1 en 3
+    //     let balloonPosY = 16.5 // tussen 16 en 17.50
+    //     let balloonPosZ = Math.random() * -1.75 + -1.25; // tussen -1.25 en -3
+    //     console.log(balloonPosZ);
+
+    //     let balloons = ["#ballonBlauw", "#ballonOranje", "#ballonGroen", "#ballonGeel"]
+    //     let randomBalloon = balloons[Math.floor(Math.random() * 4)]
+
+    //     balloon = document.createElement("a-entity")
+    //     balloon.setAttribute("position", `${balloonPosX} ${balloonPosY} ${balloonPosZ}`)
+    //     balloon.setAttribute("collada-model", randomBalloon)
+
+    //     document.getElementById("blokhutBalloons").appendChild(balloon)
+    // }
+    if (numBalloons == 9) {
+        let animation=document.createElement("a-animation")
+        animation.setAttribute("attribute", "position")
+        animation.setAttribute("to", "0 1000 0")
+        animation.setAttribute("dur", "50000")
+        document.getElementById("blokhutBalloons").appendChild(animation)
     }
     else {
-    	console.log("Add ballonnen");
-    	balloon = document.createElement("a-box")
-    	balloon.setAttribute("color", "#000")
-        balloon.setAttribute("position", "0 14 -4")
+        let triggerPosX = Math.random() * 6 -3; // tussen 3 en -3 // tussen 3 en -3
+        let triggerPosY = 12 // staat vast
+        let triggerPosZ = Math.floor(Math.random() * 3) + 0.6; // tussen 0.6 en 3
 
-        document.getElementById("blokhut").appendChild(balloon);
+        let balloonPosX = Math.random() * 2 + 1; //tussen 1 en 3
+        let balloonPosY = 16.5 // tussen 16 en 17.50
+        let balloonPosZ = Math.random() * -1.75 + -1.25; // tussen -1.25 en -3
+        console.log(balloonPosZ);
+
+        let balloons = ["#ballonBlauw", "#ballonOranje", "#ballonGroen", "#ballonGeel"]
+        let randomBalloon = balloons[Math.floor(Math.random() * 4)]
+
+        balloon = document.createElement("a-entity")
+        balloon.setAttribute("position", `${balloonPosX} ${balloonPosY} ${balloonPosZ}`)
+        balloon.setAttribute("collada-model", randomBalloon)
+
+        document.getElementById("blokhutBalloons").appendChild(balloon)
+
         numBalloons += 1
-        console.log(numBalloons);
+
+        document.getElementById("trigger").setAttribute("position", `${triggerPosX} ${triggerPosY} ${triggerPosZ}`)
     }
 }
