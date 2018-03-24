2c17f018aa9ff3cd82d7fff92060fd1485a25627
martijnbrands <nijtrambra@hotmail.com>
2018-03-24 15:35:32 +0100

    Fix balloons and trigger overlapping

--- js/script.js
+++ js/script.js
@@ -37,43 +37,51 @@ function checkActive() {
 	}
 }
 
-
-
-
 let numBalloons = 0
+let currentBalloonPosition = 0;
+let currentTriggerPosition = 0;
 
 function addBalloon() {
-    if (numBalloons == 3) {
-        document.getElementById("blokhutBalloons").setAttribute("sound", "src:assets/sounds/floating_sound.mp3;volume:25")
+    let triggerPosition = ["2.5, 11.3 0", "0, 11.80 0.30",  "-2, 11.28, -1.30", "3, 11.26, -0.90", "-1.85, 11.28, -2.10", "-4, 10.81, -1.20"]
+
+    let balloonPosition = ["2.60, 16.50, -0.70", "3.00, 16.50, -2.76", "1.03, 16.50, -3.13", "0.58, 16.50, -1.09"]
+
+    let balloons = ["#ballonBlauw", "#ballonRood", "#ballonOranje", "#ballonGroen", "#ballonGeel"]
+    
+    let randomBalloon = balloons[Math.floor(Math.random() * 4)]
+
+    balloon = document.createElement("a-entity")
+
+    balloon.setAttribute("position", balloonPosition[currentBalloonPosition]);
+
+    currentBalloonPosition++;
+
+    balloon.setAttribute("collada-model", randomBalloon)
+
+    document.getElementById("blokhutBalloons").appendChild(balloon)
+
+    numBalloons++;
+
+    document.getElementById("trigger").setAttribute("position", triggerPosition[currentTriggerPosition])
+
+    currentTriggerPosition++;
+
+    if(numBalloons == 4) {
+        levelComplete();
+    }
+}
+
+function levelComplete() {
+    document.getElementById("blokhutBalloons").setAttribute("sound", "src:assets/sounds/floating_sound.mp3;volume:25")
         
-        let animation = document.createElement("a-animation")
-        animation.setAttribute("attribute", "position")
-        animation.setAttribute("to", "0 1000 0")
-        animation.setAttribute("dur", "50000")
-        document.getElementById("blokhutBalloons").appendChild(animation)
+    let animation = document.createElement("a-animation")
+    animation.setAttribute("attribute", "position")
+    animation.setAttribute("to", "0 1000 0")
+    animation.setAttribute("dur", "50000")
+    document.getElementById("blokhutBalloons").appendChild(animation)
 
-        document.getElementById("trigger").setAttribute("visible", "false");
+    document.getElementById("trigger").setAttribute("visible", "false");
 
-        document.getElementById("next-level").setAttribute("visible", "true");
-        document.getElementById("next-level").setAttribute("position", "-1.27 11.86 -4.32");
-    }
-    else {
-        let triggerPosition = ["0, 11.80 0.30", "-2, 11.28, -1.30", "2.5, 11.3 0", "3, 11.26, -0.90", "-1.85, 11.28, -2.10", "-4, 10.81, -1.20"]
-
-        let balloonPosition = ["2.60, 16.50, -0.70", "3.00, 16.50, -2.76", "1.03, 16.50, -3.13", "0.58, 16.50, -1.09"]
-
-        let balloons = ["#ballonBlauw", "#ballonRood", "#ballonOranje", "#ballonGroen", "#ballonGeel"]
-       
-        let randomBalloon = balloons[Math.floor(Math.random() * 4)]
-
-        balloon = document.createElement("a-entity")
-        balloon.setAttribute("position", balloonPosition[Math.floor(Math.random() * 4)])
-        balloon.setAttribute("collada-model", randomBalloon)
-
-        document.getElementById("blokhutBalloons").appendChild(balloon)
-
-        numBalloons += 1
-
-        document.getElementById("trigger").setAttribute("position", triggerPosition[Math.floor(Math.random() * 4)])
-    }
+    document.getElementById("next-level").setAttribute("visible", "true");
+    document.getElementById("next-level").setAttribute("position", "-1.27 11.86 -4.32");
 }
